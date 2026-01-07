6434eb028a75708b88002b6eadaf57355fa39f00
bennettbijumathew <bennettbijumathew@gmail.com>
2026-01-07 19:33:14 +1100

    fix: removed the extra containers

--- src/routes/_user/planner/index.svelte
+++ src/routes/_user/planner/index.svelte
@@ -2,9 +2,9 @@
     import { plannerState } from "@/stores/planner/index.svelte";
 </script>
 
-<main>
+<main class="flex-1 p-4">
     <p>planner</p>
-    
+
     {#each plannerState.planners as planner}
         {planner.name}
     {/each}
