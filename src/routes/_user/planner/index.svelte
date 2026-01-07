d4d742c5619a562f965cbbe3f8938e1d12b7a5ff
bennettbijumathew <bennettbijumathew@gmail.com>
2026-01-07 19:49:23 +1100

    change: added a planner list

--- src/routes/_user/planner/index.svelte
+++ src/routes/_user/planner/index.svelte
@@ -1,11 +1,21 @@
 <script>
-    import { plannerState } from "@/stores/planner/index.svelte";
+    import { auth } from "@/lib/config/firebase";
+    import { planners } from "@/stores/planner/index.svelte";
 </script>
 
 <main class="flex-1 p-4">
-    <p>planner</p>
+    <h1 class="font-bold text-lg pb-4">planner</h1>
 
-    {#each plannerState.planners as planner}
-        {planner.name}
-    {/each}
+    <section class="flex flex-col gap-y-4">
+        {#each planners.list as planner}
+            <div class="flex justify-between border p-2">
+                <div>
+                    <p> {planner.name} </p>
+                    <p class="text-sm font-light"> {Object.keys(planner.users)} </p>
+                </div>
+
+                <input type="checkbox" defaultchecked={planner.visible}>
+            </div>
+        {/each}
+    </section>
 </main>
--- src/stores/planner/index.svelte.ts
+++ src/stores/planner/index.svelte.ts
@@ -3,15 +3,15 @@ import { auth, db } from "@/lib/config/firebase.ts"
 import { collection, onSnapshot, query, Unsubscribe as FirestoreUnsub, orderBy } from "firebase/firestore";
 import { Unsubscribe as AuthUnsub } from "firebase/auth";
 
-class PlannerState {
-    planners: any[] = $state([])
+class PlannerStore {
+    list: any[] = $state([])
     unsubFromAuth: AuthUnsub | null = null
     unsubFromQuery: FirestoreUnsub | null = null
 
     constructor() {
         this.unsubFromAuth = onAuthStateChanged(auth, (user) => {
             const userId = user?.uid ?? ""
-            this.planners = []
+            this.list = []
 
             this.unsubFromQuery?.()
             this.unsubFromQuery = null
@@ -21,11 +21,13 @@ class PlannerState {
             // This query gets a list of planners that are correlated to the user
             const q = query(collection(db, "planners"), orderBy(`users.${userId}`));
 
+            // This snapshot sets the planner list while adding a visible attribute for each user 
             this.unsubFromQuery = onSnapshot(q, (querySnapshot) => {
-                this.planners = querySnapshot.docs.map((doc) => { 
+                this.list = querySnapshot.docs.map((doc) => { 
                     return { 
                         name: doc.data().name,
-                        users: doc.data().users
+                        users: doc.data().users,
+                        visible: doc.data().users[userId]
                     }
                 })
             })            
@@ -33,4 +35,4 @@ class PlannerState {
     }   
 }
 
-export const plannerState = new PlannerState();
+export const planners = new PlannerStore();
