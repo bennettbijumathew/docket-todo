aa0516dfe89bb309478bfb88a905fddd2ad8085b
bennettbijumathew <bennettbijumathew@gmail.com>
2026-02-09 20:30:20 +1100

    addition: changed styling of the planner select component

--- src/components/planner/planner-select.svelte
+++ src/components/planner/planner-select.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { ChevronDown } from "@lucide/svelte";
     import { colors } from "@/lib/helpers/color";
     import { type Task } from "@/lib/task/type";
     import { type Planner } from "@/lib/planner/type";
@@ -7,9 +6,9 @@
     const { planners, task }: { planners: Planner[], task: Task } = $props();
     let searchInput: string = $state("")
 
-    // This sets a list of planners that are selected by the task. 
+    // This sets an array to a list of planners that are selected and unselected by the task. 
     // This array changes on a state variable changing such as task.planners or planners
-    const selectedPlanners = $derived(() => {
+    const taskPlanners = $derived(() => {
         const plannerIds = new Set(task.planners);
 
         // Returns a planners alongside a selected attribute which sees if task has selected the planner
@@ -19,40 +18,36 @@
         }));
     });
 
-    const searchedPlanners = $derived(() => selectedPlanners().filter((item) => 
+    // Using the search input, the list of planners are filtered and then sorted 
+    const searchedPlanners = $derived(() => taskPlanners().filter((item) => 
         item.name.toLowerCase().includes(searchInput) === true
+    ).sort((a, b) => 
+        Number(b.selected) - Number(a.selected)
     ))
 </script>
 
-<div>
-    {#each searchedPlanners() as planner}
-        <div class="flex border border-background-300">
-            <div class={`bg-${colors[planner.color]} p-0.5`}></div>
-            <p class="ml-2"> {planner.name} </p>
-        </div>
-    {/each}
-</div>
+<input 
+    type="text" 
+    placeholder="Search for Planners" 
+    class="flex w-full border border-background-300 p-1 rounded-lg" 
+    bind:value={searchInput}
+>
 
-<div class="flex justify-between border border-background-300 rounded-lg p-1 w-full">
-    <input type="text" placeholder="Search for Planners" class="flex-1" bind:value={searchInput}>
+{#if searchedPlanners().length > 0}
+    <div class="border border-background-300 rounded-lg p-1 w-full mt-2 max-h-38 overflow-y-auto">
+        {#each searchedPlanners() as planner}
+            <button 
+                class="flex w-full justify-between border-l-4 first:rounded-tl-sm last:rounded-bl-sm border-{colors[planner.color]} items-center hover:bg-background-50 transition-colors cursor-pointer"
+            >
+                <p class="ml-1"> {planner.name} </p>
 
-    <ChevronDown class=""></ChevronDown>
-</div>
-
-<div>
-    {#each searchedPlanners() as planner}
-        <div class="flex p-2 justify-between items-center hover:bg-background-50 transition-colors cursor-pointer border-b border-background-300">
-            <div class="flex">
-                <div class={`bg-${colors[planner.color]} p-0.5`}></div>
-                <p class="ml-2"> {planner.name} </p>
-            </div>
-
-            <input 
-                type="checkbox" 
-                id={planner.id} 
-                checked={planner.selected}
-                class="size-4 accent-text-300"
-            >   
-        </div>
-    {/each}
-</div>
+                <input 
+                    type="checkbox" 
+                    id={planner.id} 
+                    checked={planner.selected}
+                    class="size-4 accent-text-300 mr-1"
+                >   
+            </button>
+        {/each}
+    </div>
+{/if}
