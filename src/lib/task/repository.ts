1129f6824a29e5f24878e36e096753f7df0ee6a0
bennettbijumathew <bennettbijumathew@gmail.com>
2026-02-14 12:33:05 +1100

    addition: added guard clauses to create tasks

--- src/lib/task/repository.ts
+++ src/lib/task/repository.ts
@@ -24,6 +24,13 @@ export class TaskRepository {
 
     // This adds a new task into the tasks database
     public async createTask(newTask: NewTaskData): Promise<void> {
+        // A guard clause to prevent a new task being added if there is no name
+        // or planners attached to the task.
+        if (newTask.name.trim() == "" || newTask.planners.length <= 0) {
+            return 
+        }
+
+        // Grabs the tasks collection from Firestore and adds a new document using the converter. 
         const newTaskRef = collection(db, "tasks").withConverter(taskConverter)
                 
         await addDoc(newTaskRef, {
