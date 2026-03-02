bc125a6b3d987581f086e53348dad7c5482b1a27
bennettbijumathew <bennettbijumathew@gmail.com>
2026-03-02 12:45:24 +1100

    change: created a simple header for tauri

--- src/app.svelte
+++ src/app.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import { Router } from "sv-router";
 	import "sv-router/generated";
-    import { authController } from "@/lib/auth/controller";
-    import { authStore } from "@/lib/auth/store.svelte";
     import { getCurrentWindow } from '@tauri-apps/api/window';
     import { X, Maximize2, Minimize2, Minus } from '@lucide/svelte';
-    import { isActive } from 'sv-router/generated';
 
     interface Route {
         name: string,
@@ -21,18 +18,6 @@
     
     let maximizedState: boolean = $state(false) 
 
-    async function handleUserALogin() {
-        await authController.logInWithEmail(import.meta.env.VITE_ACCOUNT_TEST_A_EMAIL, import.meta.env.VITE_ACCOUNT_TEST_PASSWORD);
-    }
-
-    async function handleUserBLogin() {
-        await authController.logInWithEmail(import.meta.env.VITE_ACCOUNT_TEST_B_EMAIL, import.meta.env.VITE_ACCOUNT_TEST_PASSWORD);
-    }
-
-    function handleLogOut() {
-        authController.logOut();
-    }
-
     function isTauri(): boolean {
         return typeof window !== "undefined" && "__TAURI_INTERNALS__" in window;
     }
@@ -44,71 +29,45 @@
     }
 </script>
 
-<header data-tauri-drag-region class="flex justify-between p-4 bg-background">
-    <div class="gap-x-4 flex items-center">
-        <h1 class="font-title font-extrabold text-lg"> Docket </h1>
-        
-        <div class="gap-x-4 flex px-2 pt-0.5 items-center">
+{#if isTauri() === true}
+    <header data-tauri-drag-region class="flex items-center justify-between bg-background mb-4">
+        <div class="flex gap-x-2 ml-4">
+            <p class="text-xs select-none"> Logo </p>
+
             {#each routes as route}
-                {#if isActive(route.link)}
-                    <a href={route.link} class="border-b-2 border-content-900">
-                        {route.name}
-                    </a>
-                {:else}
-                    <a href={route.link} class="border-b-2 border-background-100 hover:border-background-300 transition-colors">
-                        {route.name}
-                    </a>
-                {/if}
+                <a href={route.link} class="text-xs pl-2 select-none">
+                    {route.name}
+                </a>
             {/each}
         </div>
 
-        <div class="gap-x-4 flex px-2 pt-0.5 items-center">
-            <button onclick={handleUserALogin} class="border-b-2 border-background-100 hover:border-background-300 transition-colors">
-                USER A
-            </button>
-
-            <button onclick={handleUserBLogin} class="border-b-2 border-background-100 hover:border-background-300 transition-colors">
-                USER B
-            </button>
-
-            <button onclick={handleLogOut} class="border-b-2 border-background-100 hover:border-background-300 transition-colors">
-                LOG OUT
-            </button>
-
-            <p class="border-b-2 border-background-100">
-                {authStore.getUser()?.email}
-            </p>
-        </div>
-    </div>
-    
-    {#if isTauri() == true}
-        <div class="gap-x-6 flex items-center">
+        <div class="flex items-center transition-colors">
             <button 
                 onclick={getCurrentWindow().minimize}
-                class="cursor-pointer"
+                class="cursor-pointer hover:bg-background-200 px-2 py-1.5"
             >
-                <Minus class="size-5 text-content-900 hover:text-content-600 transition-colors"/>  
+                <Minus class="size-4 text-content-900 hover:text-content-600"/>  
             </button>
 
             <button 
                 onclick={() => { getCurrentWindow().toggleMaximize(); isWindowMaximized() }}
-                class="cursor-pointer"
+                class="cursor-pointer hover:bg-background-200 px-2 py-1.5"
             > 
                 {#if maximizedState == true}
-                    <Minimize2 class="size-4 text-content-900 hover:text-content-600 transition-colors"/>    
+                    <Minimize2 class="size-4 text-content-900"/>    
                 {:else}
-                    <Maximize2 class="size-4 text-content-900 hover:text-content-600 transition-colors"/>    
+                    <Maximize2 class="size-4 text-content-900"/>    
                 {/if}
             </button>
             
             <button 
                 onclick={getCurrentWindow().close}
-                class="cursor-pointer"
+                class="cursor-pointer hover:bg-red-500 px-2 py-1.5"
             > 
-                <X class="size-5 text-content-900 hover:text-content-600 transition-colors"/>    
+                <X class="size-4 text-content-900"/>    
             </button>
         </div>
-    {/if}
-</header>
+    </header>
+{/if}
 
 <Router/>
