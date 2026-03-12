4a67ebd24d1a63de267ad7d6ee92577b28d40f92
bennettbijumathew <bennettbijumathew@gmail.com>
2026-03-12 13:29:25 +1100

    change: shifted app controller start an outer layout so that sessions don't end on leaving planner and task page

--- src/routes/_user/layout.svelte
+++ src/routes/_user/layout.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-    import { appController } from '@/lib/app/controller';
     import { authStore } from '@/lib/auth/store.svelte';
     import { ArrowRight } from '@lucide/svelte';
     import { navigate } from 'sv-router/generated';
-	import { onDestroy, onMount, type Snippet } from 'svelte';  
+	import { type Snippet } from 'svelte';  
     
 	let { children }: { children: Snippet } = $props();
-
-    // On the page being mounted to the DOM, the authentication controller starts.
-    onMount(() => {
-        appController.start()
-    })
-
-    // On the page being unmounted from the DOM, the authentication controller stops.
-    onDestroy(() => {
-        appController.stop()
-    })
 </script>
 
 <!-- This is the loading page -->
