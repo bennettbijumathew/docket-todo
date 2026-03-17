9e65a098cf0f9c9caeff884c66f492653b439f75
bennettbijumathew <bennettbijumathew@gmail.com>
2026-03-17 11:55:54 +1100

    change: updated the planner editor to match task editor

--- src/components/ui/inputs/color-picker.svelte
+++ src/components/ui/inputs/color-picker.svelte
@@ -29,48 +29,46 @@
         <p class="rounded-lg">{value.charAt(0).toUpperCase()}{value.slice(1)}</p>
     </Select.Trigger>
 
-    <Select.Portal>
-        <Select.Content 
-            sideOffset={offset}
-            class="bg-background border border-background-300 rounded-lg max-h-70"
-        >
-            <!-- Scroll up button for the menu -->
-            <Select.ScrollUpButton class="flex justify-center p-2 pb-0 bg-background rounded-lg">
-                <ArrowUp class="size-6 hover:bg-background-100 p-1 rounded-lg"/>
-            </Select.ScrollUpButton>
-            
-            <!-- The main select menu -->
-            <Select.Viewport class="w-(--bits-select-anchor-width) p-2 rounded-lg">
-                <!-- On pressing the color button, the onChange function provided by the caller is used -->
-                {#each Object.entries(colors) as [id]}
-                    <Select.Item 
-                        value={id}
-                        class="flex justify-between items-center hover:bg-background-100 p-1 px-2 cursor-pointer rounded-lg"
-                    >
-                        <!-- Showcases a ticked checkbox  -->
-                        {#if id === value}
-                            <p> {id.charAt(0).toUpperCase()}{id.slice(1)} </p>
-                            
-                            <div class="bg-{colors[id as ColorKey]} size-4 flex justify-center items-center rounded-xs">
-                                <Check 
-                                    class="size-3 text-content-900"
-                                    strokeWidth={5}
-                                />
-                            </div>
-                        <!-- Showcases a unchecked checkbox  -->
-                        {:else}
-                            <p> {id.charAt(0).toUpperCase()}{id.slice(1)} </p>
+    <Select.Content 
+        sideOffset={offset}
+        class="bg-background border border-background-300 rounded-lg max-h-70"
+    >
+        <!-- Scroll up button for the menu -->
+        <Select.ScrollUpButton class="flex justify-center p-2 pb-0 bg-background rounded-lg">
+            <ArrowUp class="size-6 hover:bg-background-100 p-1 rounded-lg"/>
+        </Select.ScrollUpButton>
+        
+        <!-- The main select menu -->
+        <Select.Viewport class="w-(--bits-select-anchor-width) p-2 rounded-lg">
+            <!-- On pressing the color button, the onChange function provided by the caller is used -->
+            {#each Object.entries(colors) as [id]}
+                <Select.Item 
+                    value={id}
+                    class="flex justify-between items-center hover:bg-background-100 p-1 px-2 cursor-pointer rounded-lg"
+                >
+                    <!-- Showcases a ticked checkbox  -->
+                    {#if id === value}
+                        <p> {id.charAt(0).toUpperCase()}{id.slice(1)} </p>
+                        
+                        <div class="bg-{colors[id as ColorKey]} size-4 flex justify-center items-center rounded-xs">
+                            <Check 
+                                class="size-3 text-content-900"
+                                strokeWidth={5}
+                            />
+                        </div>
+                    <!-- Showcases a unchecked checkbox  -->
+                    {:else}
+                        <p> {id.charAt(0).toUpperCase()}{id.slice(1)} </p>
 
-                            <div class="border-2 border-{colors[id as ColorKey]} size-4 flex justify-center items-center rounded-xs"></div>
-                        {/if} 
-                    </Select.Item>
-                {/each}
-            </Select.Viewport>
+                        <div class="border-2 border-{colors[id as ColorKey]} size-4 flex justify-center items-center rounded-xs"></div>
+                    {/if} 
+                </Select.Item>
+            {/each}
+        </Select.Viewport>
 
-            <!-- Scroll down button for the menu -->
-            <Select.ScrollDownButton class="flex justify-center p-2 pt-0 bg-background rounded-lg">
-                <ArrowDown class="size-6 hover:bg-background-100 p-1 rounded-lg"/>
-            </Select.ScrollDownButton>
-        </Select.Content>
-    </Select.Portal>
+        <!-- Scroll down button for the menu -->
+        <Select.ScrollDownButton class="flex justify-center p-2 pt-0 bg-background rounded-lg">
+            <ArrowDown class="size-6 hover:bg-background-100 p-1 rounded-lg"/>
+        </Select.ScrollDownButton>
+    </Select.Content>
 </Select.Root>
--- src/routes/_user/planner/index.svelte
+++ src/routes/_user/planner/index.svelte
@@ -1,74 +1,64 @@
 <!-- CODE -->
 <script lang="ts">
-    import { authStore } from "@/lib/auth/store.svelte";
-    import { plannerStore } from "@/lib/planner/store.svelte";
-    import { type NewPlannerData, type Planner } from "@/lib/planner/type";
-    import { plannerRepo } from "@/lib/planner/repository";
-    import { Plus } from "@lucide/svelte";
-    import { type ColorKey } from "@/components/util/color";
-    import ColorPicker from "@/components/ui/inputs/color-picker.svelte";
+    import { type Planner } from "@/lib/planner/type";
     import PlannerEditor from "@/components/ui/planner/editor/planner-editor.svelte";
     import Sidebar from "@/components/ui/layout/sidebar.svelte";
-    import PlannerItem from "@/components/ui/planner/list/parts/planner-item.svelte";
     import Main from "@/components/ui/layout/main.svelte";
     import PlannerContainer from "@/components/ui/planner/list/parts/planner-container.svelte";
     import PlannerListByVisible from "@/components/ui/planner/list/planner-list-by-visible.svelte";
     import PlannerInputs from "@/components/ui/planner/list/parts/planner-inputs.svelte";
+    import Editor from "@/components/ui/layout/editor.svelte";
+    import { searchParams } from "sv-router";
+    import { paramKeys, paramValues } from "@/components/util/routes";
 
+    // // These variables represent the current planner that is open on the modal
+    // // and the state of the edit modal being open
+    // let selectedPlanner: Planner | null = $state(null)
+    // let isEditModalOpen: boolean = $state(false)
 
-    // This variable represents the input data used to create a new planner
-    let newPlanner: NewPlannerData = $state({
-        name: "",
-        users: {},
-        color: "red" as ColorKey
-    })
-   
-    // This is a function for adding a new planner and resetting the inputs
-    function addNewPlanner(): void {
-        newPlanner.users = {
-            [authStore.getUserId()]:  false
-        }
+    // // This function open and closes the edit modal.
+    // function toggleEditModal(newPlanner: Planner | null) {
+    //     // If the user clicks on the same planner or the planner doesn't exist
+    //     // Then the modal is hidden from the users' view 
+    //     if (newPlanner === selectedPlanner) {
+    //         isEditModalOpen = false
+    //         selectedPlanner = null
+    //         return; 
+    //     }
 
-        plannerRepo.createPlanner(newPlanner)
-        
-        newPlanner = {
-            name: "",
-            users: {},
-            color: "red" as ColorKey
-        }
-    }
-
+    //     // If the guard clauses are passed, then the new planner is set with the view being open.
+    //     selectedPlanner = newPlanner
+    //     isEditModalOpen = true
+    // }
 
     // These variables represent the current planner that is open on the modal
-    // and the state of the edit modal being open
     let selectedPlanner: Planner | null = $state(null)
-    let isEditModalOpen: boolean = $state(false)
 
-    // This function open and closes the edit modal.
-    function toggleEditModal(newPlanner: Planner | null) {
-        // If the user clicks on the same planner or the planner doesn't exist
-        // Then the modal is hidden from the users' view 
-        if (newPlanner === selectedPlanner) {
-            isEditModalOpen = false
-            selectedPlanner = null
-            return; 
+    // This function opens the planner editor. 
+    function openEditor(planner: Planner | null) {
+        // If the user has clicked on the same planner, the editor closes
+        if (selectedPlanner == planner) {
+            closeEditor()
         }
-
-        // If the guard clauses are passed, then the new planner is set with the view being open.
-        selectedPlanner = newPlanner
-        isEditModalOpen = true
+        // If a different planner is selected, the planner is opened
+        else if (planner !== null) {
+            selectedPlanner = planner
+            
+            // Replaces the "edit" search parameter with a true value.
+            searchParams.delete(paramKeys["editor"])
+            searchParams.append(paramKeys["editor"], paramValues["editor"].open)
+        }
     }
+
+    // Closes the editor
+    function closeEditor() {
+        selectedPlanner = null
+        
+        // Deletes the current "edit" parameter.
+        searchParams.delete(paramKeys["editor"])
+    }   
+
 </script>
-
-<!-- COMPONENT: This is the snippet used to show a list of planners -->
-{#snippet listOfPlanners(list: Planner[])}
-    <main class="flex flex-col divide-y divide-background-100">
-        {#each list as planner}
-            <PlannerItem planner={planner}></PlannerItem>
-        {/each}
-    </main>
-{/snippet}
-
 
 <Sidebar/>
 
@@ -82,54 +72,20 @@
     </h2>
 
     <PlannerContainer>
-        <PlannerListByVisible/>
+        <PlannerListByVisible
+            onPlannerSelect={openEditor}
+        />
     </PlannerContainer>
     
     <PlannerInputs/>
 </Main>
 
-<!-- VIEW: This is what is shown on the arrival of the page -->
-<main class="flex-1 flex p-4 pt-0 gap-x-4 min-h-0 bg-background">
-    <section class="flex flex-col gap-y-4 flex-4 border border-background-300 rounded-xl p-4">
-        <h2 class="font-default font-semibold text-xl text-center">Planner</h2>
-
-        <!-- This area renders a list of planners -->
-        <div class="flex-1 flex flex-col min-h-0 gap-y-4 overflow-y-auto"> 
-            {@render listOfPlanners(plannerStore.getList())}
-        </div>
-
-        <!-- This area is the place to add planners -->
-        <form
-            class="flex border border-background-300 focus-within:outline focus-within:outline-background-500 rounded-lg p-1.5"
-            onsubmit={(e) => { 
-                e.preventDefault(); 
-                addNewPlanner();
-            }}
-        >
-            <div class="flex-1 flex items-center">
-                <button 
-                    class="py-2 px-2 hover:bg-background-100 rounded-lg cursor-pointer mr-1" 
-                    type="submit"
-                >
-                    <Plus class="size-4"/>
-                </button>
-
-                <input 
-                    type="text" 
-                    class="outline-none flex-1"
-                    placeholder="Enter a new planner.."
-                    aria-required="true"
-                    required
-                    bind:value={newPlanner.name}
-                >
-            </div>
-
-            <ColorPicker 
-                value={newPlanner.color}
-                onChangeFn={(newColor) => newPlanner.color = newColor as ColorKey}
-            />
-        </form>
-    </section>
-
-    <PlannerEditor plannerId={selectedPlanner?.id ?? null} toggleFn={() => toggleEditModal(selectedPlanner)}/>
-</main>
+<Editor
+    header="Edit Planner"
+    onClose={closeEditor}
+>
+    <PlannerEditor 
+        planner={selectedPlanner} 
+        toggleFn={closeEditor}
+    />
+</Editor>
