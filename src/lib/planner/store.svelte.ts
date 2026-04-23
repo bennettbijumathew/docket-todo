3116ff4a2f2fd5edc351d4cabacd6d9bcdff9dd8
bennettbijumathew <bennettbijumathew@gmail.com>
2026-04-23 20:25:50 +1000

    refactor: added new planner store class which is more readable

--- src/lib/planner/store.svelte.ts
+++ src/lib/planner/store.svelte.ts
@@ -1,6 +1,40 @@
 import { SvelteSet } from "svelte/reactivity";
 import { Planner } from "./type";
 
+export class PlannerStore {
+    #list: Planner[] = $state([])
+    #visiblePlanners: Planner[] = $derived(this.#list.filter((planner) => planner.visible === true))
+    #hiddenPlanners: Planner[] = $derived(this.#list.filter((planner) => planner.visible === false))
+    
+    set all(newList: Planner[]) {
+        this.#list = newList;
+    }
+
+    get all() {
+        return this.#list
+    }
+
+    get visible() {
+        return this.#visiblePlanners;
+    }
+
+    get hidden() {
+        return this.#hiddenPlanners;
+    }
+
+    getItemsById({idList, includeHidden}: {idList: SvelteSet<string>, includeHidden: boolean}) {
+        // Returns all planners that are a part of the id list. 
+        if (includeHidden == true) {            
+            return this.all.filter((planner) => idList.has(planner.id))
+        }
+        // Returns all of the visible planners that are a part of the id list. 
+        else {
+            return this.visible.filter((planner) => idList.has(planner.id))
+        }
+    }   
+}
+
+
 export class PlannerDataStore {
     private list: Planner[];
 
@@ -46,3 +80,6 @@ export class PlannerDataStore {
 }
 
 export const plannerStore = new PlannerDataStore()
+
+
+export const planners = new PlannerStore()
