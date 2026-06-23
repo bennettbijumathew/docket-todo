154bde6083b0d7642fb10d6276cb1bebada6cf7c
bennettbijumathew <bennettbijumathew@gmail.com>
2026-06-23 15:42:18 +1000

    refactor: removed usages of tasks.incomplete and tasks.complete as it is not used

--- src/lib/task/store.svelte.ts
+++ src/lib/task/store.svelte.ts
@@ -2,8 +2,6 @@ import { type TaskSort, type Task } from "@/lib/task/type";
 
 export class TaskStore {
     #list: Task[] = $state([])
-    #completeTasks: Task[] = $derived(this.#list.filter((item) => item.completed === true))
-    #incompleteTasks: Task[] = $derived(this.#list.filter((item) => item.completed === false))
     #sort: TaskSort = $state("completed")
 
     set all(newList: Task[]) {
@@ -13,15 +11,6 @@ export class TaskStore {
     get all() {
         return this.#list
     }
-
-    get complete () {
-        return this.#completeTasks
-    }
-
-    get incomplete () {
-        return this.#incompleteTasks
-    }
-
     set sortType (type: TaskSort) {
         this.#sort = type; 
     }
